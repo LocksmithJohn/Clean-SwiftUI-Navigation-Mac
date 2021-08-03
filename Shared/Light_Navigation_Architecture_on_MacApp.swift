f141c4b505c676b6c3f79a69cb7a9d98c3e04eb1
LocksmithJohn <slusarzjan@gmail.com>
2021-08-03 08:01:41 +0200

    Initial Commit

---
+++ Shared/Light_Navigation_Architecture_on_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Light_Navigation_Architecture_on_MacApp.swift
+//  Shared
+//
+//  Created by User on 03/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Light_Navigation_Architecture_on_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
